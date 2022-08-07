5f64a7479e8263a957396e73952bda8c38b48b47
Viktoriia755 <prostokvashino755@gmail.com>
2022-08-07 19:33:15 +0300

    Search Engine is added

--- src/app.js
+++ src/app.js
@@ -50,8 +50,20 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "7743c755808b8f2fbdff94cce411c783";
-let cityname = "London";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "7743c755808b8f2fbdff94cce411c783";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("London");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
