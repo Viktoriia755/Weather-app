1b77a2ee960b538d0352995927084504496a1777
Viktoriia755 <prostokvashino755@gmail.com>
2022-08-07 15:38:21 +0300

    Description is added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-let apiKey = "346b39f6aa7acee58b3437a1fb89c2e7";
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let countryElement = document.querySelector("#country");
+  countryElement.innerHTML = response.data.sys.country;
+
+  let pressureElement = document.querySelector("#pressure");
+  pressureElement.innerHTML = response.data.main.pressure;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#windspeed");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
+let apiKey = "7743c755808b8f2fbdff94cce411c783";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
