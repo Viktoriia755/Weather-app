da5e6cb45537fd832a018115af7c9683f4b36b84
Viktoriia755 <prostokvashino755@gmail.com>
2022-08-14 11:14:39 +0300

    Blocks of Weather Forecast are added

--- src/app.js
+++ src/app.js
@@ -20,6 +20,36 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day}, ${hours}:${minutes}`;
 }
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = ` <div class= "row" > `;
+
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+   
+  
+              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img src="" alt="image" />
+                <div class="weather-forecast-temperature">
+                  <span class="weather-forecast-temperature-max">18 </span>
+                  <span class="weather-forecast-temperature-min"> 12 </span>
+                </div>
+              </div>
+          
+  
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   celsiusLink.classList.remove("active");
@@ -95,3 +125,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("London");
+
+displayForecast();
